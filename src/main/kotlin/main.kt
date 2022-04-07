92c3cbc31cf32f6af0d3f1d3dc22376799f1670d
Viktor Anikin <Avka3d@mail.ru>
2022-04-07 10:29:48 +0500

    code editing

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -7,43 +7,41 @@ const val TODAY = DAY * 2
 const val YESTERDAY = TODAY * 3
 
 fun main() {
+    agoToText()
+}
+
+fun agoToText() {
     val seconds = readLine()!!.toInt()
-    val minute = seconds / 60
-    val hour = minute / 60
-    val textHour = endOfHours(hour)
-    val textMinute = endOfMinutes(minute)
-    val agoToTexn = timeInterval(seconds, textHour, hour, textMinute, minute)
-    println("$agoToTexn")
+    println("${timeInterval(seconds)}")
 }
 
 fun timeInterval(
     seconds: Int,
-    textHour: String,
-    hour: Int,
-    textMinute: String,
-    minute: Int
+    minute: Int = seconds / 60,
+    hour: Int = minute / 60
 ): String = when (seconds) {
     in 0..60 -> "был(а) в сети только что"
-    in MINUTE + 1..HOUR -> "был(а) в сети $minute $textMinute назад"
-    in HOUR + 1..DAY -> "был(а) в сети $hour $textHour назад"
+    in MINUTE + 1..HOUR -> "был(а) в сети $minute ${endOfMinutes(minute)} назад"
+    in HOUR + 1..DAY -> "был(а) в сети $hour ${endOfHours(hour)} назад"
     in DAY + 1..TODAY -> "был(а) в сети сегодня"
     in TODAY + 1..YESTERDAY -> "был(а) в сети вчера"
     else -> "был(а) в сети давно"
 }
 
-fun endOfHours(hour: Int): String = when {
-    hour == 1 || hour == 21 -> "час"
-    hour == 2 || hour == 3 || hour == 4 ||
-            hour == 22 || hour == 23 || hour == 24 -> "часа"
+fun endOfHours(hour: Int): String = when (hour) {
+    1, 21 -> "час"
+    in 2..4, in 22..24 -> "часа"
     else -> "часов"
 }
 
-fun endOfMinutes(minute: Int): String = when {
-    minute >= 5 && minute < 21 -> "минут"
-    minute == 1 || minute % 10 == 1 -> "минуту"
-    minute == 2 || minute == 3 || minute == 4 ||
-            minute % 10 == 2 || minute % 10 == 3 || minute % 10 == 4 -> "минуты"
-    else -> "минут"
+fun endOfMinutes(minute: Int): String = when (minute) {
+    in 5..20,
+    in 25..30,
+    in 35..40,
+    in 45..50,
+    in 55..60 -> "минут"
+    1, 21, 31, 41, 51 -> "минуту"
+    else -> "минуты"
 }
 
 
